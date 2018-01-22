02bed436743b583f0defd961a0ead93b6bf8d9b9
Jodi Huang <jodijh@mit.edu>
2018-01-22 13:28:03 -0500

    routing engine

--- atlas_of_innovation/settings.py
+++ atlas_of_innovation/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'atlas_of_innovation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'atlas',
+        'USER': 'jodi',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
