4d01046f0c892d6eb764e3f94c3b11593a3a589a
Jodi Huang <jodijh@mit.edu>
2018-02-01 16:14:42 -0500

    converted mako templates to django templates for static pages (for loops not working)

--- application/templates/snippets/head.mako
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-  <title>Ananse Group</title>
-	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700|Source+Sans+Pro:200,300,300i,400,600" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="${request.static_url('atlas_of_innovation:static/css/stylesheet.css')}" />
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script type='text/javascript' src="${request.static_url('atlas_of_innovation:static/js/page.js')}"></script>
-</head>
-<body>
--- application/views/static_pages.py
+++ application/views/static_pages.py
@@ -5,60 +5,62 @@ from django.shortcuts import render
 def home(request):
     return render(
         request,
-        'makermap.mako',
+        'makermap.html',
     )
 
 
 def map(request):
     return render(
         request,
-        'makermap.mako',
+        'makermap.html',
     )
 
 
 def about(request):
     return render(
         request,
-        'static/about.mako',
+        'static/about.html',
     )
 
 
 def goals(request):
     return render(
         request,
-        'static/goals.mako',
+        'static/goals.html',
     )
 
 
 def userDocs(request):
     return render(
         request,
-        'static/user-documentation.mako',
+        'static/user-documentation.html',
     )
 
 
 def devDocs(request):
     return render(
         request,
-        'static/developer-documentation.mako',
+        'static/developer-documentation.html',
     )
 
 
 def wiki(request):
+    """
     with open('countries.json') as json_file:    
         data = json.load(json_file)
         c_list=[]
         for p in data['country']:
             c_list.append(p['countryName'])
         return {'countries':c_list}
+    """
     return render(
         request,
-        'wiki.mako',
+        'wiki.html',
     )
 
 
 def contribute(request):
     return render(
         request,
-        'static/contribute.mako',
+        'static/contribute.html',
     )
