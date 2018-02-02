46c36e1006f1a6eddef9a4b0734adffc24894df0
Jodi Huang <jodijh@mit.edu>
2018-02-01 22:02:27 -0500

    finished converting mako templates to django templates for static pages

--- application/views/static_pages.py
+++ application/views/static_pages.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 
 
 def home(request):
+    type = ['All', 'Ecovillage', 'Event', 'Hub', 'Virtual', 'Workshop']
+    theme = ['Agriculture', 'Appropriate Technology', 'Art and Culture', 'Biology', 'Design', 'Education', 'Food', 'Materials', 'Media', 'Politics', 'Science', 'Youth']
+    filter = ['Type', 'Theme']
+    context = {'type':type, 'theme':theme, 'filter':filter}
     return render(
         request,
         'makermap.html',
+        context,
     )
 
 
@@ -45,17 +50,17 @@ def devDocs(request):
 
 
 def wiki(request):
-    """
     with open('countries.json') as json_file:    
         data = json.load(json_file)
         c_list=[]
         for p in data['country']:
             c_list.append(p['countryName'])
-        return {'countries':c_list}
-    """
+    themes = ['Agriculture', 'Appropriate Technology', 'Biology', 'Design', 'Education', 'Food', 'Materials', 'Media', 'Politics', 'Science', 'Youth']
+    context = {'countries':c_list, 'themes':themes}
     return render(
         request,
         'wiki.html',
+        context,
     )
 
 
