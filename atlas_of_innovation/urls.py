046436c5e177e29dd3e421a316918e9e277402c3
Lyla Fischer <lylafisch@gmail.com>
2018-03-23 11:57:45 -0400

    removed the admin app, because we don't use it.

--- atlas_of_innovation/urls.py
+++ atlas_of_innovation/urls.py
@@ -20,6 +20,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('application.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
