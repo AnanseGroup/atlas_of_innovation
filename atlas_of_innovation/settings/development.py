7fb30b137043ae856cd98f0bc4e74a6e722dcc2d
Lyla Fischer <lylafisch@gmail.com>
2018-04-03 11:35:51 -0400

    elasticbeanstalk: Initial commit for deployment files

---
+++ atlas_of_innovation/settings/development.py
@@ -0,0 +1,17 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'l^o92!g+^lp459xkndhnc620hutn8lb%fueku1n4pxe+2vch1i'
+
+# SECURITY_WARNING: don't use these keys in production 
+RECAPTCHA_PUBLIC_KEY = '6Lf_uiQUAAAAALicrb1JbiITWdIdOTuQzIHnnodJ'
+RECAPTCHA_PRIVATE_KEY = '6Lf_uiQUAAAAABHjt9ryEqxIOPTSMDeEQSrz7O-q'
+NOCAPTCHA = True
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
