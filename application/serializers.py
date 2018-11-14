c862372ac65c27a9b86f3dc0c6cab58b6e113e48
Pedro Quiroz <pedro@canteradigital.mx>
2018-11-14 15:52:22 -0600

    Added validated and recently added properties to the Space model. Modified API calls and views to react to this properties (404 error in case someone tries to edit a validated space). Modified the spaces profile and list templates with badges for validated and recently modified properties. Modified CSV importer so it can read a trusted_source boolean field or the validation_status field

--- application/serializers.py
+++ application/serializers.py
@@ -3,7 +3,9 @@ from rest_framework import serializers
 from django_countries.serializers import CountryFieldMixin
 
 class SpaceSerializer(CountryFieldMixin, serializers.ModelSerializer):
-
+    validated = serializers.ReadOnlyField()
+    recently_updated = serializers.ReadOnlyField()
+    
     def __init__(self, *args, fields=None, **kwargs):
 
         super(SpaceSerializer, self).__init__(*args, **kwargs)
@@ -11,6 +13,8 @@ class SpaceSerializer(CountryFieldMixin, serializers.ModelSerializer):
         if fields:
             allowed = set(fields)
             existing = set(self.fields.keys())
+            existing.add('validated')
+            existing.add('recently_updated')
             for field_name in existing - allowed:
                 self.fields.pop(field_name)
 
--- application/views/space_views.py
+++ application/views/space_views.py
@@ -6,9 +6,11 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib import messages
+from django.http import Http404
 
 from application.models import Space
 from application.models.spaces import SpaceForm
+import datetime
 
 def space_profile(request, id):
     space = Space.objects.get(id=id)
@@ -29,6 +31,16 @@ class SpaceEdit(UpdateView):
     form_class = SpaceForm
     model = Space
     template_name = 'space_edit.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(SpaceEdit, self).get_context_data(**kwargs)
+        space = self.object
+        ''' (Pedro) Part of #93 If space is validated we disallow accessing 
+            the edit form I think a 404 error would suffice
+        '''
+        if space.validated:
+            raise Http404
+        return context
 
 edit_space = SpaceEdit.as_view()
 
