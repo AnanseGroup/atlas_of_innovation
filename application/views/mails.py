76be20f38b2d40f5bc505092dd6a281e9bea50bd
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-01-25 09:27:42 -0600

    added post-office to requirements, use standar head in all templates and added mail template

--- application/views/mails.py
+++ application/views/mails.py
@@ -14,17 +14,12 @@ from post_office import mail
 
 	
 def to_moderator(DataCreditLog):
-EmailTemplate.objects.create(
-	    name='onchange_notification',
-	    subject='Space Changes',
-	    content='Hi {{ name }},',
-	    html_content=' some changes had made in spacemade by <strong>{{ credit }}</strong> <br/>Check details<a href={{url}}>HERE</a>',
-		)
-url = "http://localhost:8000/space/"+str(DataCreditLog.space_id)
-credit =str(DataCreditLog.credit)
-mail.send(
-	   ['orlandosalvadorcamarillomoreno@gmail.com'], #List of email addresses also accepted 'noreply@atlasofinnovation.com',
-	   template= 'onchange_notification',
-	   context={'url':url, 'credit':credit},)
-	  # some changes was made in a space<br/> by <strong>{{ credit }}</strong><br/> if you want see the new space information click <a href="{{url}}">HERE</a>',
-	  # )
+   
+	url = "http://52.37.166.83/space/"+str(DataCreditLog.space_id)
+	credit =str(DataCreditLog.credit)
+	mail.send(
+		   ['orlandosalvadorcamarillomoreno@gmail.com'], #List of email addresses also accepted  ana@parthenontech.com
+			'noreply@atlasofinnovation.com',
+		   template= 'onchange_notification',
+		   context={'url':url, 'credit':credit,'name':'Ana'},)
+		 
