ec8f96ce450ec58e627104c58456ef76eba4c333
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-01-25 11:52:53 -0600

    styles

--- application/views/mails.py
+++ application/views/mails.py
@@ -15,10 +15,10 @@ from post_office import mail
 	
 def to_moderator(DataCreditLog):
    
-	url = "http://52.37.166.83/space/"+str(DataCreditLog.space_id)
+	url = "http://localhost:8000/space/"+str(DataCreditLog.space_id)
 	credit =str(DataCreditLog.credit)
 	mail.send(
-		   ['orlandosalvadorcamarillomoreno@gmail.com'], #List of email addresses also accepted  ana@parthenontech.com
+		   ['ana@parthenontech.com'], #List of email addresses also accepted  
 			'noreply@atlasofinnovation.com',
 		   template= 'onchange_notification',
 		   context={'url':url, 'credit':credit,'name':'Ana'},)
