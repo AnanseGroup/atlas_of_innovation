24505a900a0d29fbe5ab9eac6525ae9fbbcb5d5a
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-02-28 05:05:44 -0600

    remove helptext from signup  userform

--- application/models/user.py
+++ application/models/user.py
@@ -30,7 +30,11 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
 
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
