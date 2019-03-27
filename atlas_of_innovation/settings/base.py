4c648b5ba3b28226c64347ebe9cc2d16b8f0823f
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-03-27 11:34:24 -0600

    settings for mail

--- atlas_of_innovation/settings/base.py
+++ atlas_of_innovation/settings/base.py
@@ -130,3 +130,13 @@ STATIC_URL = '/static/'
 URL="52.37.166.83/"
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 TEMPLATE_STRING_IF_INVALID="invalid field name"
+EMAIL_BACKEND = 'post_office.EmailBackend'
+#use gmailserver
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'atlasofinnovation@gmail.com'
+EMAIL_HOST_PASSWORD = '4tl4sofinnovation'
+EMAIL_PORT = 587
+POST_OFFICE = {
+    'DEFAULT_PRIORITY': 'now'
+}
