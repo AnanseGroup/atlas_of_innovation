bda4959aaed22e55f93a7f0102b8f2963469fab4
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-04-02 09:13:59 -0600

    corrected error in Ismoderator function

--- application/views/space_views.py
+++ application/views/space_views.py
@@ -1148,7 +1148,7 @@ def IsOwner(user_id,space_id):
             return False
 def IsModeratorOfSpace(user,province,country):
                 if user.moderator.is_moderator:
-                    if user.province and province:
+                    if user.moderator.province and province:
                         return True
                 if user.moderator.is_country_moderator:
                     if user.country == country:
