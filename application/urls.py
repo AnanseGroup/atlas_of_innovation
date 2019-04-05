ca80dcf761e523f6fa73c309df769b87bd3c08c0
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-04-05 16:46:47 -0600

    url login logout problems

--- application/urls.py
+++ application/urls.py
@@ -41,10 +41,10 @@ urlpatterns = [
     path(r'admin/', admin.site.urls),
     
     path('signup', views.signup, name='signup'),
-    re_path(r'login/', auth_views.LoginView),
-    path(r'logout/', auth_views.LogoutView,{'next_page': '/'}),
+    re_path(r'login/', auth_views.LoginView.as_view(), name='login'),
+    path(r'logout/', auth_views.LogoutView.as_view(),{'next_page': '/'}, name='logout'),
     path('reset_password',views.password_reset,name='password_reset'),
-    path('reset_done',auth_views.PasswordResetDoneView,name='password_reset_done'),
+    path('reset_done',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.password_reset_confirm,name='password_reset_confirm'),
     #activation email
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
