8fd85776a46c56956ad16f19c3cb510df62b743e
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-04-05 16:14:38 -0600

    upgrade django to 2.1.5

--- application/urls.py
+++ application/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf.urls import url
 from django.contrib import admin
@@ -41,10 +41,10 @@ urlpatterns = [
     path(r'admin/', admin.site.urls),
     
     path('signup', views.signup, name='signup'),
-    path(r'login/', auth_views.login, name='login'),
-    path(r'logout/', auth_views.logout,{'next_page': '/'}, name='logout'),
-    path(r'reset_password',views.password_reset,name='password_reset'),
-    path('reset_done',auth_views.password_reset_done,name='password_reset_done'),
+    re_path(r'login/', auth_views.LoginView, name='login'),
+    path(r'logout/', auth_views.LogoutView,{'next_page': '/'}, name='logout'),
+    path('reset_password',views.password_reset,name='password_reset'),
+    path('reset_done',auth_views.PasswordResetDoneView,name='password_reset_done'),
     url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.password_reset_confirm,name='password_reset_confirm'),
     #activation email
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
